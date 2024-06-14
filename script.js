0198fd2c329831921df3027274e31f4d0a10cc1c
Harshita-Tomar <harshitatomar28@gmail.com>
2024-06-14 14:26:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+gsap.from(".top", 1.6, {
+	delay: 0.4,
+	height: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".bottom", 1.6, {
+	delay: 0.4,
+	height: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".account", 1, {
+	x: -150,
+	delay: 1,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".logo", 1, {
+	x: -20,
+	delay: 1.2,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".menu-btn", 1, {
+	x: -20,
+	delay: 1.4,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+
+gsap.from(".search", 0.8, {
+	x: -20,
+	delay: 1.6,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".product-img", 1.5, {
+	y: 10,
+	delay: 1.9,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".product-title", 2, {
+	y: 200,
+	delay: 2,
+	opacity: 0,
+	ease: Circ.easeInOut
+});
+gsap.from(".product-title span", 2, {
+	y: -50,
+	delay: 3,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".product-subtitle", 2, {
+	y: 200,
+	delay: 3.5,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".tag-line", 1, {
+	x: -20,
+	delay: 3,
+	opacity: 0,
+	ease: Expo.easeInOut
+});
+gsap.from(".media ul li", 1, {
+	x: -120,
+	delay: 4,
+	opacity: 0,
+	stagger: 0.08,
+	ease: Power3.easeInOut
+});
+gsap.from(".arrow-icon ul li", 1, {
+	x: 50,
+	delay: 4.4,
+	opacity: 0,
+	stagger: 0.08,
+	ease: Power3.easeInOut
+});
+
+
+
